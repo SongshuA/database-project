b12ebbbee15487cb1fe794c09189b53d0c3c0f47
polarwk <16302010035@fudan.edu.cn>
2019-01-04 14:53:35 +0800

    777

--- src/test.java
+++ src/test.java
@@ -8,16 +8,6 @@ import java.util.List;
 
 public class test {
     public static void main(String[] args) {
-        MonthBillManager monthBillManager = new MonthBillManager();
-        DateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        format.setLenient(false);
-        try{
-            Timestamp begin = new Timestamp(format.parse("2019-1-1 12:12:12").getTime());
-            Timestamp end = new Timestamp(format.parse("2019-1-5 10:10:00").getTime());
-            List<MonthBill> monthBills = monthBillManager.get(4,begin, end);
-            System.out.println(monthBills.size());
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
+
     }
 }
